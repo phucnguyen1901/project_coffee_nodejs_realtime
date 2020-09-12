2ac33a09c555313ce4d020c68a22ea16684f368b
Phuc Nguyen <kobaogiopo2@gmail.com>
2020-09-12 15:22:51 +0700

    add page cart,auth

--- server.js
+++ server.js
@@ -6,14 +6,29 @@ const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    res.render('home')
-})
-
 // set Template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
+
+// asset
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req,res) =>{
+    res.render('customers/cart')
+})
+
+app.get('/login', (req,res) =>{
+    res.render('auth/login')
+})
+
+app.get('/register', (req,res) =>{
+    res.render('auth/register')
+})
 
 
 app.listen(port, ()=> console.log("Server was stated with port "+port))
